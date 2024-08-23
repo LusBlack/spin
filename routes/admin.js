efd6801e4242f8043d48c1c7e9d545426c31c1be
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-08-23 16:49:41 +0100

    merged with pug frontend and established routes

---
+++ routes/admin.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+    res.render('shop', { pageTitle: 'product' })
+});
+
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+
+    res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
