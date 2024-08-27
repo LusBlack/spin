bdaad42c4d0524b9992187ed83fb6c9ef8bd5bf5
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-08-27 01:52:59 +0100

    switched to ejs and fixed pathing issues

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.set('view engine', 'pug')
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -20,7 +22,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-    res.status(404).render('404', { pageTitle: 'not found'} );
+    res.status(404).render('404', { 
+        pageTitle: 'not found',
+        path: '404'
+    });
 });
 
 
