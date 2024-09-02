6ce6c320af6aa51740b6515d3120c362af29a802
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-09-02 23:35:44 +0100

    cleaning up

--- models/product.js
+++ models/product.js
@@ -1,26 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
+const getProductsFromFile = () => {
+  const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+  );
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      console.log("Error reading file in fetchAll():", err);
+      return cb([]);  // Return early if there's an error
+    }
+
+    try {
+      console.log("File content:", fileContent.toString());  // Log the file content for debugging
+      const products = JSON.parse(fileContent);
+      cb(products);
+    } catch (parseErr) {
+      console.log("Error parsing JSON in fetchAll():", parseErr);
+      cb([]);
+    }
+  }); 
+}
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
+    getProductsFromFile();
     const p = path.join(
       path.dirname(require.main.filename),  // Use require.main.filename
       'data',
       'products.json'
     );
     fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        try {
-          products = JSON.parse(fileContent);
-        } catch (parseErr) {
-          console.log("Error parsing JSON in save():", parseErr);
-        }
-      }
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         if (err) {
@@ -31,25 +47,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        console.log("Error reading file in fetchAll():", err);
-        return cb([]);  // Return early if there's an error
-      }
-
-      try {
-        console.log("File content:", fileContent.toString());  // Log the file content for debugging
-        const products = JSON.parse(fileContent);
-        cb(products);
-      } catch (parseErr) {
-        console.log("Error parsing JSON in fetchAll():", parseErr);
-        cb([]);
-      }
-    });
+    getProductsFromFile(cb);
   }
 };
