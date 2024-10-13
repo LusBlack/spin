2d84a52d0f2ad2ba6e4f9281d2f6660663c29186
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-10-13 19:42:57 +0100

    fixed routing issues and get product detail bug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const notFound = require('./controllers/404');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(notFound.pageNotFound);
 
